c5b6d0190e3ea22935e223e4c10cb2936c08012e
git-hgjeong <code.hgjeong@gmail.com>
2021-03-02 07:05:56 +0900

    라우터 적용

---
+++ routes.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Page1 from './Page1';
+//import Page2 from './Page2';
+//import Page3 from './Page3';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes:[
+        { path: '/page1', component: Page1 }
+    ]
+});
