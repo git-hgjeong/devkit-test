43b21380e508f51b9a05510052e399b13b16985a
git-hgjeong <code.hgjeong@gmail.com>
2021-03-14 18:17:15 +0900

    트리작업중

---
+++ src/work/TreeMenu.vue
@@ -0,0 +1,65 @@
+<template>
+  <li><a href="javascript:void(0);">{{ item.name }}</a>
+    <ul v-if="isFolder">
+      <tree-item
+        v-for="(child, index) in item.children"
+        :key="index"
+        :item="child"
+      ></tree-item>
+    </ul>
+  </li>
+</template>
+<script>
+
+export default {  
+  props: {
+    item: Object
+  },
+  data: function () {
+    return {
+      isOpen: false
+    }
+  },
+  computed: {
+    isFolder: function () {
+      return this.item.children &&
+        this.item.children.length
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.isFolder) {
+        this.isOpen = !this.isOpen
+      }
+    },
+    makeFolder: function () {
+      if (!this.isFolder) {
+      	this.$emit('make-folder', this.item)
+        this.isOpen = true
+      }
+    }
+  }    
+}
+
+			/*
+			https://kr.vuejs.org/v2/examples/tree-view.html
+			//Depth Start
+
+
+											//1st Depth
+											<li><a href="javascript:void(0);">지수</a>
+												<ul>
+
+													//2nd Depth
+													<li> <a href="javascript:void(0);">주가지수</a>
+														<ul>
+															//last Depth
+															<li class="CI-MDI-MENU-NO-CHILD"><a href="javascript:void(0);" data-menu-id="MDC0201010101">전체지수 시세</a></li>
+														</ul>
+													</li>
+													
+												</ul>
+											</li>
+
+			*/
+</script>
