fcfc60eb8ca2bd3e595603d5013656e4cbb185f3
git-hgjeong <code.hgjeong@gmail.com>
2021-03-21 15:20:16 +0900

    메뉴데이터 이동 및 mdi데이터 병합

--- src/work/LnbWork.vue
+++ src/work/LnbWork.vue
@@ -47,91 +47,11 @@
 </template>
 <script>
 
-
-const menu_test = [
-				{"data-menu-id":"MDC0201", "name":"기본 통계"},
-					{"data-menu-id":"MDC020101", "name":"지수"},
-						{"data-menu-id":"MDC02010101", "name":"주가지수"},
-							{"data-menu-id":"MDC0201010101", "name":"전체지수 시세"},
-							{"data-menu-id":"MDC0201010102", "name":"전체지수 등락률"},
-							{"data-menu-id":"MDC0201010103", "name":"개별지수 시세 추이"},
-							{"data-menu-id":"MDC0201010104", "name":"전체지수 기본정보"},
-							{"data-menu-id":"MDC0201010105", "name":"개별지수 종합정보"},
-							{"data-menu-id":"MDC0201010106", "name":"지수구성종목"},
-							{"data-menu-id":"MDC0201010107", "name":"PER/PBR/배당수익률"},
-
-						{"data-menu-id":"MDC02010102", "name":"채권지수"},
-							{"data-menu-id":"MDC0201010201", "name":"전체지수 시세"},
-							{"data-menu-id":"MDC0201010202", "name":"개별지수 시세 추이"}
-            ];
-
-const menu = [
-				{"data-menu-id":"MDC0201", "name":"기본 통계", isOpen:true},
-					{"data-menu-id":"MDC020101", "name":"지수", isOpen:true},
-						{"data-menu-id":"MDC02010101", "name":"주가지수", isOpen:true},
-							{"data-menu-id":"MDC0201010101", "name":"전체지수 시세", isOpen:false},
-							{"data-menu-id":"MDC0201010102", "name":"전체지수 등락률", isOpen:false},
-							{"data-menu-id":"MDC0201010103", "name":"개별지수 시세 추이", isOpen:false},
-							{"data-menu-id":"MDC0201010104", "name":"전체지수 기본정보", isOpen:false},
-							{"data-menu-id":"MDC0201010105", "name":"개별지수 종합정보", isOpen:false},
-							{"data-menu-id":"MDC0201010106", "name":"지수구성종목", isOpen:false},
-							{"data-menu-id":"MDC0201010107", "name":"PER/PBR/배당수익률", isOpen:false},
-
-						{"data-menu-id":"MDC02010102", "name":"채권지수", isOpen:false},
-							{"data-menu-id":"MDC0201010201", "name":"전체지수 시세", isOpen:false},
-							{"data-menu-id":"MDC0201010202", "name":"개별지수 시세 추이", isOpen:false},
-						
-						{"data-menu-id":"MDC02010103", "name":"파생 및 기타지수", isOpen:false},
-							{"data-menu-id":"MDC0201010301", "name":"전체지수 시세", isOpen:false},
-							{"data-menu-id":"MDC0201010302", "name":"전체지수 등락률", isOpen:false},
-							{"data-menu-id":"MDC0201010303", "name":"개별지수 시세 추이", isOpen:false},
-							{"data-menu-id":"MDC0201010304", "name":"전체지수 기본정보", isOpen:false},
-							{"data-menu-id":"MDC0201010305", "name":"개별지수 종합정보", isOpen:false},
-
-                {"data-menu-id":"MDC020102", "name":"주식", isOpen:false},
-					{"data-menu-id":"MDC02010201", "name":"종목시세", isOpen:false},
-						{"data-menu-id":"MDC0201020101", "name":"전종목 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020102", "name":"전종목 등락률", isOpen:false},
-						{"data-menu-id":"MDC0201020103", "name":"개별종목 시세 추이", isOpen:false},
-						{"data-menu-id":"MDC0201020104", "name":"개별종목 시세 추이(월)", isOpen:false},
-					{"data-menu-id":"MDC02010202", "name":"종목정보", isOpen:false},
-					{"data-menu-id":"MDC0201020201", "name":"전종목 기본정보", isOpen:false},
-					{"data-menu-id":"MDC0201020202", "name":"전종목 지정내역", isOpen:false},
-					{"data-menu-id":"MDC0201020203", "name":"개별종목 종합정보", isOpen:false},
-					{"data-menu-id":"MDC02010203", "name":"거래실적", isOpen:false},
-						{"data-menu-id":"MDC0201020301", "name":"투자자별 거래실적", isOpen:false},
-						{"data-menu-id":"MDC0201020302", "name":"투자자별 거래실적(개별종목)", isOpen:false},
-						{"data-menu-id":"MDC0201020303", "name":"투자자별 순매수상위종목", isOpen:false},
-						{"data-menu-id":"MDC0201020304", "name":"대량매매(전일)", isOpen:false},
-						{"data-menu-id":"MDC0201020305", "name":"프로그램매매", isOpen:false},
-					{"data-menu-id":"MDC02010204", "name":"기타증권", isOpen:false},
-						{"data-menu-id":"MDC0201020401", "name":"REITs 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020402", "name":"뮤추얼펀드 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020403", "name":"선박투자회사 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020404", "name":"인프라투융자회사 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020405", "name":"수익증권 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020406", "name":"신주인수권증권 시세", isOpen:false},
-						{"data-menu-id":"MDC0201020407", "name":"신주인수권증서 시세", isOpen:false},
-
-					{"data-menu-id":"MDC02010205", "name":"세부안내", isOpen:false},
-						{"data-menu-id":"MDC0201020501", "name":"상장회사 상세검색", isOpen:false},
-						{"data-menu-id":"MDC0201020502", "name":"PER/PBR/배당수익률(개별종목)", isOpen:false},
-						{"data-menu-id":"MDC0201020503", "name":"외국인보유량 추이", isOpen:false},
-						{"data-menu-id":"MDC0201020504", "name":"외국인보유량(개별종목)", isOpen:false},
-						{"data-menu-id":"MDC0201020505", "name":"업종별 분포", isOpen:false},
-						{"data-menu-id":"MDC0201020506", "name":"업종분류 현황", isOpen:false},
-
-						{"data-menu-id":"MDC0201020507", "name":"대용가격", isOpen:false},
-							{"data-menu-id":"MDC020102050701", "name":"주식 대용가", isOpen:false},
-							{"data-menu-id":"MDC020102050702", "name":"수익증권 대용가", isOpen:false},
-							{"data-menu-id":"MDC020102050703", "name":"뮤추얼펀드 대용가", isOpen:false}
-            ];
-
 function setChildMenu(arrMenu, item){
 	//console.log("arrMenu:", JSON.stringify(arrMenu));
-	var mid = item["data-menu-id"];
+	var mid = item["mid"];
 	var prevMenu = arrMenu;
-	var prevMid =  prevMenu["data-menu-id"];
+	var prevMid =  prevMenu["mid"];
 	var parentMid = mid.substr(0, mid.length-2);
 	
 	if(parentMid == prevMid){
@@ -166,6 +86,9 @@ const menuAllOpenIcon = "http://data.krx.co.kr/templets/mdc/img/blit_s.png";
 const menuAllOpenText = "전체 닫기";
 
 export default {
+	props: {
+    	menu: Object,
+  	},	
 	data(){
 		return {
 			treeData: {},
@@ -203,7 +126,7 @@ export default {
 		//li > ul > li
 		var tempMenu;
 		var arrMenu = [];
-		menu.forEach(function(item, index, arr){
+		this.menu.forEach(function(item, index, arr){
 			var arrIdx = arrMenu.length - 1;
 			if(arrIdx == -1){
 				//첫배열 저장
--- src/work/Work.vue
+++ src/work/Work.vue
@@ -9,14 +9,14 @@
 		<!-- CONTAINER -->
 		<section id="container" class="container">
 			<!-- LBN -->
-			<lnb-component></lnb-component>
+			<lnb-component :menu="menuList"></lnb-component>
 			<!-- //LNB -->
 			<!-- CONTENT -->
 			<section class="content CI-MDI-WRAP" style="">
 				<!--b:s-->
 				<!-- CONTENT IN-->
 				<ul class="tab1 CI-MDI-TAB-WRAP" id="jsMdiTab">
-					<li v-for="(item, index) in midMenuList" v-bind:key="item" v-bind:class="item.tabClass"><a href="javascript:void(0);" @click="onClickMdiTap(item, index)"  v-bind:title="'['+item.mid+']'+' '+item.title" class="CI-MDI-TAB-NAME"><span>[{{item.mid}}] {{item.title}}</span><button type="button" title="닫기" class="CI-MDI-TAB-CLOSE"></button></a></li>
+					<li v-for="(item, index) in midMenuList" v-bind:key="item" v-bind:class="item.tabClass"><a href="javascript:void(0);" @click="onClickMdiTap(item, index)"  v-bind:title="'['+item.mid+']'+' '+item.title" class="CI-MDI-TAB-NAME"><span>{{item.name}}</span><button type="button" title="닫기" class="CI-MDI-TAB-CLOSE"></button></a></li>
 				</ul>
 				<section class="contents_in">
 					<div id="jsMdiContent" class="CI-MDI-CONTENT-WRAP">
@@ -48,6 +48,69 @@ import router from './routes';
 import Header from './WorkHeader';
 import LeftMenu from './LnbWork';
 
+const dataMenu = [
+				{"mid":"MDC0201", "name":"기본 통계", isOpen:true},
+					{"mid":"MDC020101", "name":"지수", isOpen:true},
+						{"mid":"MDC02010101", "name":"주가지수", isOpen:true},
+							{"mid":"MDC0201010101", "name":"전체지수 시세", isOpen:false, link:"page1"},
+							{"mid":"MDC0201010102", "name":"전체지수 등락률", isOpen:false, link:"page2"},
+							{"mid":"MDC0201010103", "name":"개별지수 시세 추이", isOpen:false, link:"page3"},
+							{"mid":"MDC0201010104", "name":"전체지수 기본정보", isOpen:false},
+							{"mid":"MDC0201010105", "name":"개별지수 종합정보", isOpen:false},
+							{"mid":"MDC0201010106", "name":"지수구성종목", isOpen:false},
+							{"mid":"MDC0201010107", "name":"PER/PBR/배당수익률", isOpen:false},
+
+						{"mid":"MDC02010102", "name":"채권지수", isOpen:false},
+							{"mid":"MDC0201010201", "name":"전체지수 시세", isOpen:false},
+							{"mid":"MDC0201010202", "name":"개별지수 시세 추이", isOpen:false},
+						
+						{"mid":"MDC02010103", "name":"파생 및 기타지수", isOpen:false},
+							{"mid":"MDC0201010301", "name":"전체지수 시세", isOpen:false},
+							{"mid":"MDC0201010302", "name":"전체지수 등락률", isOpen:false},
+							{"mid":"MDC0201010303", "name":"개별지수 시세 추이", isOpen:false},
+							{"mid":"MDC0201010304", "name":"전체지수 기본정보", isOpen:false},
+							{"mid":"MDC0201010305", "name":"개별지수 종합정보", isOpen:false},
+
+                {"mid":"MDC020102", "name":"주식", isOpen:false},
+					{"mid":"MDC02010201", "name":"종목시세", isOpen:false},
+						{"mid":"MDC0201020101", "name":"전종목 시세", isOpen:false},
+						{"mid":"MDC0201020102", "name":"전종목 등락률", isOpen:false},
+						{"mid":"MDC0201020103", "name":"개별종목 시세 추이", isOpen:false},
+						{"mid":"MDC0201020104", "name":"개별종목 시세 추이(월)", isOpen:false},
+					{"mid":"MDC02010202", "name":"종목정보", isOpen:false},
+					{"mid":"MDC0201020201", "name":"전종목 기본정보", isOpen:false},
+					{"mid":"MDC0201020202", "name":"전종목 지정내역", isOpen:false},
+					{"mid":"MDC0201020203", "name":"개별종목 종합정보", isOpen:false},
+					{"mid":"MDC02010203", "name":"거래실적", isOpen:false},
+						{"mid":"MDC0201020301", "name":"투자자별 거래실적", isOpen:false},
+						{"mid":"MDC0201020302", "name":"투자자별 거래실적(개별종목)", isOpen:false},
+						{"mid":"MDC0201020303", "name":"투자자별 순매수상위종목", isOpen:false},
+						{"mid":"MDC0201020304", "name":"대량매매(전일)", isOpen:false},
+						{"mid":"MDC0201020305", "name":"프로그램매매", isOpen:false},
+					{"mid":"MDC02010204", "name":"기타증권", isOpen:false},
+						{"mid":"MDC0201020401", "name":"REITs 시세", isOpen:false},
+						{"mid":"MDC0201020402", "name":"뮤추얼펀드 시세", isOpen:false},
+						{"mid":"MDC0201020403", "name":"선박투자회사 시세", isOpen:false},
+						{"mid":"MDC0201020404", "name":"인프라투융자회사 시세", isOpen:false},
+						{"mid":"MDC0201020405", "name":"수익증권 시세", isOpen:false},
+						{"mid":"MDC0201020406", "name":"신주인수권증권 시세", isOpen:false},
+						{"mid":"MDC0201020407", "name":"신주인수권증서 시세", isOpen:false},
+
+					{"mid":"MDC02010205", "name":"세부안내", isOpen:false},
+						{"mid":"MDC0201020501", "name":"상장회사 상세검색", isOpen:false},
+						{"mid":"MDC0201020502", "name":"PER/PBR/배당수익률(개별종목)", isOpen:false},
+						{"mid":"MDC0201020503", "name":"외국인보유량 추이", isOpen:false},
+						{"mid":"MDC0201020504", "name":"외국인보유량(개별종목)", isOpen:false},
+						{"mid":"MDC0201020505", "name":"업종별 분포", isOpen:false},
+						{"mid":"MDC0201020506", "name":"업종분류 현황", isOpen:false},
+
+						{"mid":"MDC0201020507", "name":"대용가격", isOpen:false},
+							{"mid":"MDC020102050701", "name":"주식 대용가", isOpen:false},
+							{"mid":"MDC020102050702", "name":"수익증권 대용가", isOpen:false},
+							{"mid":"MDC020102050703", "name":"뮤추얼펀드 대용가", isOpen:false}
+            ];
+
+//{mid:"41001", title:"주가지수 월별 추이", link:"page1", tabClass:"CI-MDI-TAB"},
 export default {
 	router,
     components: {
@@ -56,10 +119,8 @@ export default {
     },
 	data(){
 		return {
-			midMenuList:[ {mid:"41001", title:"주가지수 월별 추이", link:"page1", tabClass:"CI-MDI-TAB"},
-					{mid:"42001", title:"종합정보(월)", link:"page2", tabClass:"CI-MDI-TAB"},
-					{mid:"43001", title:"종합정보(일)", link:"page3", tabClass:"CI-MDI-TAB"} 
-			]
+			menuList: dataMenu,
+			midMenuList: []
 		}
 	},
 	methods: {
