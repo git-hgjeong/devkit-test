4c3fab94bb5c8d3da73374437fe2983a3a20e2e7
git-hgjeong <code.hgjeong@gmail.com>
2021-04-05 23:14:49 +0900

    체크박스 클릭 오류 수정

--- src/work/MultiCombo.vue
+++ src/work/MultiCombo.vue
@@ -3,10 +3,10 @@
       <input ref="comboInput" type="text" name="" value="" size="100" @click="showComboDiv">
       <div ref="comboDiv" v-show="comboOpen" style="position: absolute;background-color: #fff;z-index:99;padding:10px;border:1px solid #b3b7c4;" :style="{ left: '0px', top: comboTop + 'px' }" @mouseleave = "hideComboDiv">
           <ul>
-              <li><input type="checkbox" value="1"><label>테스트1</label></li>
-              <li><input type="checkbox" value="2"><label>테스트2</label></li>
-              <li><input type="checkbox" value="3"><label>테스트3</label></li>
-              <li><input type="checkbox" value="4"><label>테스트4</label></li>
+              <li><input type="checkbox" name="chk1" id="chk1" value="1"><label for="chk1">테스트1</label></li>
+              <li><input type="checkbox" name="chk2" id="chk2" value="2"><label for="chk2">테스트2</label></li>
+              <li><input type="checkbox" name="chk3" id="chk3" value="3"><label for="chk3">테스트3</label></li>
+              <li><input type="checkbox" name="chk4" id="chk4" value="4"><label for="chk4">테스트4</label></li>
           </ul>
           <button type="button" @click="doApplyCombo">적용</button>
           <button type="button" @click="hideComboDiv">닫기</button>
